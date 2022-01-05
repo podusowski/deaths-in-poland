864b142bf4cf754bb03dfc8f7496799cc4265450
Piotr Podusowski <ext-lux-pp@ad.datcon.co.uk>
2022-01-05 16:38:31 +0100

    move to fn

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 use calamine::{open_workbook, Error, RangeDeserializerBuilder, Reader, Xlsx};
 
-fn main() -> anyhow::Result<()> {
-    let mut workbook: Xlsx<_> =
-        open_workbook("data/Zgony wedИug tygodni w Polsce_2021.xlsx").unwrap();
+struct AnnualData {
+    general: Vec<Option<f64>>,
+}
+
+fn read(path: &str) -> anyhow::Result<AnnualData> {
+    let mut workbook: Xlsx<_> = open_workbook(path)?;
 
     let range = workbook
         .worksheet_range("OGÓŁEM")
         .ok_or(Error::Msg("No sheet."))??;
 
-    //for row in range.rows() {
-    //    println!("{:?}", row);
-    //}
-
-    let general = &range
+    let general = range
         .rows()
         .find(|r| {
             if let Some(s) = r[0].get_string() {
@@ -21,20 +20,16 @@ fn main() -> anyhow::Result<()> {
                 false
             }
         })
-        .ok_or(anyhow::anyhow!("no data"))?[3..];
+        .ok_or(anyhow::anyhow!("no data"))?[3..]
+        .iter()
+        .map(|v| v.get_float().or(None))
+        .collect();
 
-    println!("{:?}", general);
+    Ok(AnnualData { general })
+}
 
+fn main() -> anyhow::Result<()> {
+    let y2021 = read("data/Zgony wedИug tygodni w Polsce_2021.xlsx")?;
+    println!("{:?}", y2021.general);
     Ok(())
-
-    //let mut iter = RangeDeserializerBuilder::new().from_range(&range)?;
-
-    //if let Some(result) = iter.next() {
-    //    let (label, value): (String, f64) = result?;
-    //    assert_eq!(label, "celsius");
-    //    assert_eq!(value, 22.2222);
-    //    Ok(())
-    //} else {
-    //    Err(anyhow::anyhow!("expected at least one record but got none"))
-    //}
 }
